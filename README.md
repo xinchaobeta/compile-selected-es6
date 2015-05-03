f471c0a56643b8061be14c21edd3334a92eaecf7
xinchaobeta <xinchaobeta@hotmail.com>
2015-05-03 16:03:02 +0800

    1. add syntax check on file save 2. read .babelrc setting

--- README.md
+++ README.md
@@ -26,7 +26,18 @@ Package Control will automatically keep the package up to date with the latest v
 ![the-result-of-compiled-code](https://raw.githubusercontent.com/xinchaobeta/compile-selected-es6/master/screenshots/result.png)
 
 * Now you got the compiled code.
-* Note that you can also use the keyboard shortcut `Option+Shift+d` to compile the code without using the mouse way.
+
+>  Note that you can also use the keyboard shortcut `Option+Shift+d` to compile the code without using the mouse way.
+
+
+----------
+
+
+![check-syntax-on-save](https://raw.githubusercontent.com/xinchaobeta/compile-selected-es6/master/screenshots/check-onsave.png)
+
+* By default, the plugin will check synax error when you save the file. You can disable this feature by changing the plugin user setting `checkSyntaxOnSave` to false.
+
+
 
 ## Settings
 
@@ -34,7 +45,10 @@ Go to `Preferences > Package Settings > Compile Selected ES6 > Settings - User` 
 
 Full details in the comments in `Preferences > Package Settings > Compile Selected ES6 > Settings - Default`
 
-- Note if you changed the `stage` value in the settings, make sure you installed the lastest version of Babel whose CLI support  `--stage`
+There's two ways to change [the `stage` option](http://babeljs.io/docs/usage/options/#options). One way is using the
+plugin user setting. Another way is [creating a `.babelrc` file in the project directory](https://babeljs.io/docs/usage/babelrc/). The `.babelrc` file settings will override the plugin user settings.
+
+> Note if you changed the `stage` value in the settings, make sure you installed the latest version of Babel whose CLI support  `--stage` . 
 
 ## Issues & Feature Requests
 
--- compile_selected_es6.py
+++ compile_selected_es6.py
@@ -1,10 +1,19 @@
 from subprocess import Popen, PIPE
-import sublime, sublime_plugin, os
+import sublime, sublime_plugin, os, json
+
+def readBabelrc():
+  path = sublime.active_window().extract_variables()["folder"] + '/.babelrc'
+  try:
+    with open(path) as babelrc :
+      config = json.load(babelrc)
+  except:
+    config = {}
+  return config
 
 def isJSX(view=None):
     if view is None:
         view = sublime.active_window().active_view()
-    return 'source.js' in view.scope_name(0)
+    return 'source.js' in view.scope_name(0).split()
 
 def settings_get(name, default=None):
     # load up the plugin settings
@@ -15,10 +24,13 @@ def compile(source=''):
   env = {
     'PATH': settings_get('envPATH', os.environ.get('PATH'))
   }
-  stage = settings_get('stage', '2')
+  babelrc = readBabelrc()
+  stage = str(settings_get('stage', '2'))
+  if "stage" in babelrc :
+    stage = str(babelrc["stage"])
   # lagency babel did not support --stage arguments
   # so if a user modified default stage, then i can sure he installed the lastest version  
-  if stage is not 2:
+  if stage is not "2":
     command = ['babel', '--stage', stage]
   else:
     command = ['babel']
@@ -26,6 +38,10 @@ def compile(source=''):
   proc = Popen(command, env=env, cwd=None, stdout=PIPE, stdin=PIPE, stderr=PIPE)
   stat = proc.communicate(input=source.encode('utf-8'))
   return {"okay": proc.returncode == 0, "out": stat[0].decode('utf-8'), "err": stat[1].decode('utf-8')}
+
+def extractError(str=''):
+  str = '\n'.join(str.split("\n")[4:])
+  return str.split('    at ')[0]
 
 class Text():
   @staticmethod
@@ -49,8 +65,10 @@ class Text():
     return Text.all(view)
 
 class CompileSelectedEs6Command(sublime_plugin.TextCommand):
+
   def is_enabled(self):
       return isJSX(self.view)
+
   def run(self, edit):
     output = self.view.window().new_file()
     output.set_scratch(True)
@@ -60,4 +78,20 @@ class CompileSelectedEs6Command(sublime_plugin.TextCommand):
         output.insert(edit, 0, res["out"])
     else:
         output.insert(edit, 0, res["err"])
+
+class CaptureEditing(sublime_plugin.EventListener):
+
+  def is_enabled(self, view):
+      return isJSX(view)
+
+  def on_post_save(self, view):
+      if not self.is_enabled(view):
+          return
+      if settings_get("checkSyntaxOnSave", False):
+          res = compile(Text.get(view))
+          if res["okay"] is True:
+              sublime.status_message("Syntax is valid.")
+          else:
+              sublime.message_dialog('Syntax error: %s' % extractError(res["err"]))
+
 
