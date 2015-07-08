336c0039240ce12e45133724c5c7fe034540ae41
xinchaobeta <xinchaobeta@hotmail.com>
2015-07-08 21:42:31 +0800

    fix path bug in windows platform

--- compile_selected_es6.py
+++ compile_selected_es6.py
@@ -1,5 +1,5 @@
 from subprocess import Popen, PIPE
-import sublime, sublime_plugin, os, json
+import sublime, sublime_plugin, os, json, sys, locale
 
 def readBabelrc():
   try:
@@ -18,7 +18,7 @@ def isJSX(view=None):
     if view is None:
         view = sublime.active_window().active_view()
     scope_name_arr = view.scope_name(0).split()
-    return 'source.js' in scope_name_arr or 'source.js.jsx' in scope_name_arr
+    return 'source.js' in scope_name_arr
 
 def settings_get(name, default=None):
     # load up the plugin settings
@@ -27,26 +27,32 @@ def settings_get(name, default=None):
     return default if value is None else value
 
 def compile(source=''):
-  env = {
-    'PATH': settings_get('envPATH', os.environ.get('PATH'))
-  }
   babelrc = readBabelrc()
   stage = str(settings_get('stage', '2'))
   if "stage" in babelrc :
     stage = str(babelrc["stage"])
-  # lagency babel did not support --stage arguments
-  # so if a user modified default stage, then i can sure he installed the lastest version  
-  if stage != "2":
-    command = ['babel', '--stage', stage]
-  else:
-    command = ['babel']
+  command = ['babel', '--stage', stage]
 
-  proc = Popen(command, env=env, cwd=None, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-  stat = proc.communicate(input=source.encode('utf-8'))
-  return {"okay": proc.returncode == 0, "out": stat[0].decode('utf-8'), "err": stat[1].decode('utf-8')}
+  if sys.platform == "win32":
+    proc = Popen(command, env=None, cwd=None, stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
+
+    stat = proc.communicate(input=source.encode("utf8"))
+    return {"okay": proc.returncode == 0, "out": stat[0].decode(locale.getdefaultlocale()[1]), "err": stat[1].decode(locale.getdefaultlocale()[1])}
+
+  else:
+    env = {
+      'PATH': settings_get('envPATH', os.environ.get('PATH'))
+    }
+
+    proc = Popen(command, env=env, cwd=None, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    stat = proc.communicate(input=source.encode('utf-8'))
+    return {"okay": proc.returncode == 0, "out": stat[0].decode('utf-8'), "err": stat[1].decode('utf-8')}
 
 def extractError(str=''):
-  str = '\n'.join(str.split("\n")[4:])
+  if sys.platform == "win32":
+    str = '\r\n'.join(str.split("\r\n")[3:])
+  else:
+    str = '\n'.join(str.split("\n")[4:])
   return str.split('    at ')[0]
 
 class Text():
@@ -84,7 +90,7 @@ class CompileSelectedEs6Command(sublime_plugin.TextCommand):
     if res["okay"] is True:
         output.insert(edit, 0, res["out"])
     else:
-        output.insert(edit, 0, res["err"])
+        output.insert(edit, 0, extractError(res["err"]))
 
 class CaptureEditing(sublime_plugin.EventListener):
 
